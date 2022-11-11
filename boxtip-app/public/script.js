3cbec316b81eed6c13b81258a9bf7fd9c3e5767a
Andry <ddry.dangduck@gmail.com>
2022-11-11 21:42:20 +0700

    Update customer pages

--- boxtip-app/public/script.js
+++ boxtip-app/public/script.js
@@ -1,26 +1,28 @@
-console.log('log')
 let btn_create = document.getElementById('btn-create');
 let btn_edit = document.getElementById('btn-edit');
 let btn_save = document.getElementById('btn-save');
 let btn_discard = document.getElementById('btn-discard');
 
-if (btn_create){
-    btn_create.addEventListener('click', function(e){
+if (btn_create) {
+    btn_create.addEventListener('click', function (e) {
         location.href += '/create';
     });
 }
-if (btn_edit){
-    btn_edit.addEventListener('click', function(e){
+if (btn_edit) {
+    btn_edit.addEventListener('click', function (e) {
         location.href += '/edit';
     });
 }
-if (btn_save){
-    btn_save.addEventListener('click', function(e){
+if (btn_save) {
+    btn_save.addEventListener('click', function (e) {
         document.getElementById('btn-submit').click();
     });
 }
-if(btn_discard){
-    btn_discard.addEventListener('click', function(e){
+if (btn_discard) {
+    btn_discard.addEventListener('click', function (e) {
         location.href = location.href.replace('/edit', '');
     });
 }
+$("#check-all").click(function () {
+    $('.check-row').prop('checked', this.checked);
+});
--- boxtip-app/resources/views/customer/tree.blade.php
+++ boxtip-app/resources/views/customer/tree.blade.php
@@ -1,1 +1,46 @@
-
+@extends('layouts.app')
+@section('content')
+    @include('components.control_panel')
+    <div class="container-fluid">
+        <table class="table table-sm table-hover my-2 table-bordered w-100" style="font-size: 0.8rem">
+            <thead>
+                <tr>
+                    <th width="10"><input type="checkbox" id="check-all"></th>
+                    <th>Name</th>
+                    <th>Code</th>
+                    <th>Old Code</th>
+                    <th>Birth Date</th>
+                    <th>Gender</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Province</th>
+                    <th>City</th>
+                    <th>District</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($customers as $customer)
+                    <tr>
+                        <td><input type="checkbox" class="check-row" data-id="{{ $customer->id }}"></td>
+                        <td>{{ $customer->name }}</td>
+                        <td>{{ $customer->code }}</td>
+                        <td>{{ $customer->old_code }}</td>
+                        <td>{{ $customer->birth_date }}</td>
+                        <td>
+                            @if ($customer->is_male)
+                                Male
+                            @else
+                                Female
+                            @endif
+                        </td>
+                        <td>{{ $customer->email }}</td>
+                        <td>{{ $customer->phone }}</td>
+                        <td>{{ $customer->province }}</td>
+                        <td>{{ $customer->city }}</td>
+                        <td>{{ $customer->district }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
